c2600293fbdcbc2b9bbe20a059d8a290f15523f0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-04 12:17:31 +0200

    remove set eau

--- add-path.sh
+++ add-path.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 ARGU="$_"
-set -eau
 
 # NOTE : Run this script by sourcing it.
 if [[ $ARGU == $0 ]]; then
   echo "Run this script by sourcing it: . $(basename "$0")"
   exit 0
 fi
-
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
