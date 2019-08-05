99697a8bf291c22fa3e55a248be886471c18a72b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-05 18:47:39 +0200

    gitlab windows

--- gitlab-docker.sh
+++
@@ -1,9 +0,0 @@
-docker run --detach \
-  --hostname gitlab.athome.tld \
-  --publish 443:443 --publish 80:80 --publish 22:22 \
-  --name gitlab \
-  --restart always \
-  --volume /srv/gitlab/config:/etc/gitlab \
-  --volume /srv/gitlab/logs:/var/log/gitlab \
-  --volume /srv/gitlab/data:/var/opt/gitlab \
-  gitlab/gitlab-ce:latest
