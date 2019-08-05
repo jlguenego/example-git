8e1a2475397d7cbbd5a998883bbb04f25347c325
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-05 10:39:41 +0200

    ok

--- decompress.sh
+++ decompress.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -eau
 
-SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-. "${SOURCE_DIR}/gitlib/base.sh"
-
 rm -rf test ||.
 
 git init test
