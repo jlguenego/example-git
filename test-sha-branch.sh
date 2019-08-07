c5e6dc4aea05dddbcf29ae406591939db750f150
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-08-07 17:19:45 +0200

    sha1 ambiguous

---
+++ test-sha-branch.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eau
+
+rm -rf test ||.
+
+
+git init test
+cd test
+echo a>a.txt
+git add -A
+git commit -m c1
+
+echo b>a.txt
+git commit -am c2
+
+COMMIT_ID=$(git log -1 --pretty=oneline | cut -f 1 -d' ')
+
+echo COMMIT_ID=$COMMIT_ID
+
+git checkout -b $COMMIT_ID
+git checkout master
+git checkout $COMMIT_ID
+# see the warning about ambiguity ? ;)
+
+
+
+
